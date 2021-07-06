32dba7f8f2f5c606680500947f46e1582a834507
Lucas Franceschino <lucas.franceschino@inria.fr>
2021-07-06 13:39:34 +0200

    update

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-20.09";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     fstar-source = {
       url = "github:FStarLang/FStar";
